7f56b1f0b78b86c027b209f0f9d9ce92a0045d30
Dave Glowacki <dglo@icecube.wisc.edu>
2008-02-10 17:48:36 -0600

    Issue 2332: The great February style spam

--- src/main/java/icecube/daq/common/DAQCmdInterface.java
+++ src/main/java/icecube/daq/common/DAQCmdInterface.java
@@ -1,7 +1,7 @@
 /*
  * class: CmdInterface
  *
- * Version $Id: DAQCmdInterface.java 2334 2007-11-28 23:53:08Z ksb $
+ * Version $Id: DAQCmdInterface.java 2629 2008-02-11 05:48:36Z dglo $
  *
  * Date: March 8 2004
  *
@@ -13,52 +13,52 @@ package icecube.daq.common;
 /**
  * This class defines constants that are used through the DAQ software.
  *
- * @version $Id: DAQCmdInterface.java 2334 2007-11-28 23:53:08Z ksb $
+ * @version $Id: DAQCmdInterface.java 2629 2008-02-11 05:48:36Z dglo $
  * @author mcp
  */
 public interface DAQCmdInterface
 {
-    public static final int DAQ_MAX_NUM_STRINGS = 80;
-    public static final int DAQ_MAX_NUM_IDH = 16;
+    int DAQ_MAX_NUM_STRINGS = 80;
+    int DAQ_MAX_NUM_IDH = 16;
 
-    public static final String DAQ_PAYLOAD_INVALID_SOURCE_ID =
+    String DAQ_PAYLOAD_INVALID_SOURCE_ID =
             "payloadInvalidSourceId";
 
     // names for all component types
-    public static final String DAQ_DOMHUB =
+    String DAQ_DOMHUB =
             "domHub";
-    public static final String DAQ_STRINGPROCESSOR =
+    String DAQ_STRINGPROCESSOR =
             "stringProcessor";
-    public static final String DAQ_ICETOP_DATA_HANDLER =
+    String DAQ_ICETOP_DATA_HANDLER =
             "iceTopDataHandler";
-    public static final String DAQ_INICE_TRIGGER =
+    String DAQ_INICE_TRIGGER =
             "inIceTrigger";
-    public static final String DAQ_ICETOP_TRIGGER =
+    String DAQ_ICETOP_TRIGGER =
             "iceTopTrigger";
-    public static final String DAQ_AMANDA_TRIGGER =
+    String DAQ_AMANDA_TRIGGER =
             "amandaTrigger";
-    public static final String DAQ_GLOBAL_TRIGGER =
+    String DAQ_GLOBAL_TRIGGER =
             "globalTrigger";
-    public static final String DAQ_EVENTBUILDER =
+    String DAQ_EVENTBUILDER =
             "eventBuilder";
-    public static final String DAQ_MONITORBUILDER =
+    String DAQ_MONITORBUILDER =
             "monitorBuilder";
-    public static final String DAQ_SNBUILDER =
+    String DAQ_SNBUILDER =
             "snBuilder";
-    public static final String DAQ_TCALBUILDER =
+    String DAQ_TCALBUILDER =
             "tcalBuilder";
-    public static final String DAQ_SECONDARY_BUILDERS =
+    String DAQ_SECONDARY_BUILDERS =
             "secondaryBuilders";
-    public static final String DAQ_STRING_HUB =
+    String DAQ_STRING_HUB =
             "stringHub";
-    
-    public static final String DAQ_ONLINE_RUNSTART_FLAG =
+
+    String DAQ_ONLINE_RUNSTART_FLAG =
             "RunStart:";
-    public static final String DAQ_ONLINE_RUNSTOP_FLAG =
+    String DAQ_ONLINE_RUNSTOP_FLAG =
             "RunStop:";
-    public static final String DAQ_ONLINE_SUBRUNSTART_FLAG =
+    String DAQ_ONLINE_SUBRUNSTART_FLAG =
             "SubrunStart:";
 
-    public static final String SOURCE = "source";
-    public static final String SINK = "sink";
+    String SOURCE = "source";
+    String SINK = "sink";
 }
